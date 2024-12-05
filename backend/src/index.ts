0c095a64a4be9f9b2174f7946604ce0e90177814
Mohammed Alheraki <md7ohe@gmail.com>
2024-12-05 19:38:37 +0300

    add nodeJS with TS to the project

---
+++ backend/src/index.ts
@@ -0,0 +1,11 @@
+import http from "http";
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello, TypeScript with Node.js!\n");
+});
+
+server.listen(3000, () => {
+  console.log("Server running at http://localhost:3000/");
+});
